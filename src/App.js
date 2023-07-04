bd105eba771a91ca3cd3b0d13047243445d63d23
dominic <dominicseduraja3@gmail.com>
2023-07-04 23:37:14 +0530

    html setups

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import './App.css';
 
 function App() {
   return (
-    <>Responsive Control Pannel</>
+    <div className='sidebar'>
+        <div className='sidebar-brand'>
+          <h1><span className='lab la-accusoft'>Accusoft</span></h1>
+        </div>
+
+        <div className='sidebar-menu'>
+          <ul>
+            <li>
+              <a href='https://www.google.com'><span className='lab la igloo'></span>
+              <span>Dashboard</span></a>
+            </li>
+          </ul>
+        </div>
+    </div>
   );
 }
 
